c87411c0eb4d8a6a5be33c272d7b6125cb47383a
Rayhona Jalilova <rayhona@anur.tech>
2024-01-03 12:02:23 +0300

    created test

--- blog/blog/tests.py
+++ blog/blog/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from .models import Blog
+
+class BlogModelTests(TestCase):
+    
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="testuser",
+            email="test@test.com",
+            password="998887766test",
+        )
+
+        self.post = Blog.objects.create(
+            title="Test post",
+            description = "Test body",
+            author=self.user,
+        )
+
+    
+    def test_string_representation(self):
+        post = Blog(title="Test post")
+        self.assertEqual(str(post), post.title)
+    
+    def test_post_content(self):
+        self.assertEqual(f"{self.post.title}", "Test post")
+        self.assertEqual(f"{self.post.author}", "testuser")
+        self.assertEqual(f"{self.post.description}", "Test body")
--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Blog
 # Create your views here.
@@ -9,3 +9,7 @@ from .models import Blog
 class BlogListView(ListView):
     model = Blog
     template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = 'post_detail.html'
