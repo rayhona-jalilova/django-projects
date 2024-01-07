83c8c4a919177a77dc05822dbaf44644658f78a2
Rayhona Jalilova <rayhona@anur.tech>
2024-01-07 19:28:34 +0300

    I finished task project

---
+++ task/car.py
@@ -0,0 +1,18 @@
+class Car:
+    """Person class."""
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+
+    def get_car_name(self):
+        """Method returns name of a car."""
+        return self.name
+    
+    def get_car_color(self):
+        """Method returns color of a car."""
+        return self.color
+    
+car = Car("Mers", "black")
+print(car)
+
